63a301b6035970863f345f0cf1ac23d71dc0ce77
shaminaj-disha <stowfika88@gmail.com>
2025-03-23 19:02:26 +0600

    Fix style

--- app/root.tsx
+++ app/root.tsx
@@ -78,8 +78,8 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   return (
     <main className="min-h-screen container flex flex-col gap-2 justify-center items-center text-primary p-4">
       <img width={270} src="/images/broken-page.png" alt="Not Found" />
-      <h1 className="text-4xl">{message}</h1>
-      <p>{details}</p>
+      <h1 className="text-4xl text-center">{message}</h1>
+      <p className=" text-center">{details}</p>
       {stack && (
         <pre className="w-full p-4 overflow-x-auto mx-auto">
           <code>{stack}</code>
